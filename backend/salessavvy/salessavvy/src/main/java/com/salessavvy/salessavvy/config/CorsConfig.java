1c99438f01ce0367628ffcfa06b7c6b3c464ead2
Bhavya <kutalabhavya@gmail.com>
2026-03-09 19:55:01 +0530

    Added CORS configuration

--- backend/salessavvy/salessavvy/src/main/java/com/salessavvy/salessavvy/config/CorsConfig.java
+++ backend/salessavvy/salessavvy/src/main/java/com/salessavvy/salessavvy/config/CorsConfig.java
@@ -1,15 +1,23 @@
 package com.salessavvy.salessavvy.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
